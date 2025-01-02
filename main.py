cd7c830cdc16be373edc2b1873d80a3623608dcb
Rafal Sota <biuro@unico-group.pl>
2025-01-02 20:38:05 +0100

    correct - lower words

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ def check_palindrom(palindrom):
     pal=[]    
     pal_revers=[]
     for letter in palindrom:
-        pal.append(letter)
+        pal.append(letter.lower())
     for letter in palindrom[::-1]:
-        pal_revers.append(letter)
+        pal_revers.append(letter.lower())
     for i in range(0, len(palindrom)):
         if pal[i]!=pal_revers[i]:
             return False
@@ -12,7 +12,7 @@ def check_palindrom(palindrom):
             return True
 
 word = "PoTOp"
-if check_palindrom(word.lower()):
+if check_palindrom(word):
     print(f"\nSłowo '{word}' jest palindromem\n")
 else:
     print(f"\nSłowo '{word}' nie jest palindromem\n")
